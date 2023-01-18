198af8c6c0fe1c7c04c7ac6d6989b859e835cd35
fernandocandeiatorres <fernandotorrescandeia@gmail.com>
2023-01-18 08:48:19 -0300

    added models.entities

--- demo-dao-project/src/Main.java
+++ demo-dao-project/src/Main.java
@@ -1,7 +1,10 @@
+import model.entities.Department;
+
 import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
-
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
     }
 }
